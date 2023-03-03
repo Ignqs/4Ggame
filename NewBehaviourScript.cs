37d5d7f7b0642d368291947523afc90cfa53215e
EmiKrav <126260437+EmiKrav@users.noreply.github.com>
2023-03-03 18:09:34 +0200

    Update NewBehaviourScript.cs

--- NewBehaviourScript.cs
+++ NewBehaviourScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1af78d6ee71e7e64010c837eb85382ddb96b5f96cf4a41b4c92ff7b225f736e
-size 1927
+oid sha256:6f85698e7148148fc2febd3211466406f97503099ed2215954a48d66ea71779d
+size 1928
